65f8a25bf649d24bda000727ebc55dfead834ca2
Michael Scarpace <mscarpace@deloitte.com>
2018-06-04 14:18:25 -0400

    updating FileCopy

--- src/fileio/FileCopy.java
+++ src/fileio/FileCopy.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 public class FileCopy {
 	public static void main(String[] args) {
 		if(args.length != 2){
@@ -13,7 +14,7 @@ public class FileCopy {
 		Path pathSource = Paths.get(args[0]);
 		Path pathDestination = Paths.get(args[1]);
 		try {
-			Files.copy(pathSource, pathDestination);
+			Files.copy(pathSource, pathDestination, StandardCopyOption.REPLACE_EXISTING);
 			System.out.println("Source file copied successfully");
 		} catch (IOException e) {
 			e.printStackTrace();
