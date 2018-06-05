389a9d119771d1d664616935d12807fd3ea773d6
Michael Scarpace <mscarpace@deloitte.com>
2018-06-04 22:16:10 -0400

    adding ListFiles

---
+++ src/fileio/ListFiles.java
@@ -0,0 +1,14 @@
+package fileio;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.IOException;
+import java.util.stream.Stream;
+class ListFiles {
+	public static void main(String []args) throws IOException {
+		try(Stream<Path> entries = Files.list(Paths.get("."))) {
+			entries.forEach(System.out::println);
+		}
+	}
+}
